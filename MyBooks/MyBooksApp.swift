c51449d488369419fecab3440f04f406b72c3785
do-not-panic <schach@gmail.com>
2024-10-21 10:17:53 +0200

    Initial Commit

---
+++ MyBooks/MyBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBooksApp.swift
+//  MyBooks
+//
+//  Created by René Pfammatter on 21.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct MyBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
